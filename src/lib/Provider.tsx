94beddf3b1ba12a8c635839c6e17367e406f59e6
sumonhasan55 <wubsumon7577@gmail.comm>
2023-11-01 23:44:19 +0600

    initialize & redux & antd setup

---
+++ src/lib/Provider.tsx
@@ -0,0 +1,14 @@
+"use client"
+import { Provider } from "react-redux";
+import { store } from "./../redux/store";
+import StyledComponentsRegistry from "./AntdRegistry";
+
+const Providers = ({ children }: { children: React.ReactNode }) => {
+  return <Provider store={store}>
+    < StyledComponentsRegistry>
+      {children}
+    </StyledComponentsRegistry>
+  </Provider>;
+};
+
+export default Providers;
